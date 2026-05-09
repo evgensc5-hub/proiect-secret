acd7bd27783733a4734c336aaf5c51fec4b9884f
Eugen Scutaru <evgensc5@gmail.com>
2026-05-09 11:25:25 +0300

    Add new Troleibuz

--- src/MijlocDeTransport.java
+++ src/MijlocDeTransport.java
@@ -2,6 +2,7 @@ public class MijlocDeTransport {
     static void main() {
         System.out.println("Jetour");
         System.out.println("Avion");
+        System.out.println("Troleibuz");
     }
 
 }
